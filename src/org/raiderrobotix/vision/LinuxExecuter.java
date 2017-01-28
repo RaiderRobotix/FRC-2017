323b9e1ef464ea5c4e4a29db9839cfc77d1d3da3
Raider Robotix <frcteam25@gmail.com>
2017-01-28 15:19:15 -0500

    Added Execution Code
    In Linux Executer

--- src/org/raiderrobotix/vision/LinuxExecuter.java
+++ src/org/raiderrobotix/vision/LinuxExecuter.java
@@ -1,15 +1,19 @@
 package org.raiderrobotix.vision;
 
+import java.io.IOException;
+
 public abstract class LinuxExecuter {
 
 	// Execute commands on Linux OS command line.
 
-	public static void execute(String command) {
-		// TODO: copy over
+	public static void execute(String command) throws IOException {
+		Runtime.getRuntime().exec(command);
 	}
 
-	public static void executeAll(String[] cmds) {
-		// TODO: copy over
+	public static void executeAll(String[] cmds) throws IOException {
+		for (String i : cmds) {
+			execute(i);
+		}
 	}
 
 }
